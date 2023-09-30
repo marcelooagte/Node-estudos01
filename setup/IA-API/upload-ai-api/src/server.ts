29906a5a63dfdd00b9040cf56fefe2e5311463ec
marcelooagte <marcelooagte@gmail.com>
2023-09-30 05:12:30 -0300

    Finalizando aula 01

--- setup/IA-API/upload-ai-api/src/server.ts
+++ setup/IA-API/upload-ai-api/src/server.ts
@@ -1,12 +1,19 @@
 import {fastify} from 'fastify';
+import {fastifyCors } from '@fastify/cors'
 import { prisma } from './lib/prisma';
 import { getAllPromptsRoute } from './routers/get-all-prompts';
 import { uploadVideoRoute } from './routers/upload-video';
-import { createTranscriptionRouter } from './routers/create-transcription';
+import { createTranscriptionRoute } from './routers/create-transcription';
+import {generationAICompletionRoute} from './routers/generate-ai-completion';
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*', 
+})
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
-app.register(createTranscriptionRouter);
+app.register(createTranscriptionRoute);
+app.register(generationAICompletionRoute)
 
 app.listen({
     port:3333,
