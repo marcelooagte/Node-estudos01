d821e1ff29ae697242f6365bee8d8f68f42743af
marcelooagte <marcelooagte@gmail.com>
2023-09-30 05:21:20 -0300

    upload-ai-web

---
+++ setup/IA-API/upload-ai-web/src/App.tsx
@@ -0,0 +1,37 @@
+import { Button } from "./components/ui/button";
+import {Github} from 'lucide-react'
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+
+export function App() {
+   return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b ">
+        <h1 className="text-xl font-bold ">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground ">Desenvolvido com 💜 no NLW da RocketSeat </span>
+          
+          <Separator orientation="vertical" className="h-6"/> 
+          
+          <Button variant="outline">
+          <Github className="w-4 h-4️⃣ mr-2"/>
+            Github
+            </Button>
+        </div>
+      </div>
+
+    <main className="flex-1 p-6 flex gap-6 ">
+      <div className="flex flex-col flex-1 gap-4 ">
+        <div className="grid grid-rows-2 gap-4 flex-1">
+          <Textarea/>
+          <Textarea/>
+        </div>
+        <p className="text-sm text-muted-foreground">Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{Transcription}'} </code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.   </p>
+      </div>
+      <aside className="w-80"></aside>
+    </main>
+
+    </div>
+  )
+}
+
