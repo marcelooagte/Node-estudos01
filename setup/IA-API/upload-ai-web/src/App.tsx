97a440c9c24985ab50d0b2eabfb8bde7aaadddd4
marcelooagte <marcelooagte@gmail.com>
2023-10-01 17:56:55 -0300

    adding elements - front web IA

--- setup/IA-API/upload-ai-web/src/App.tsx
+++ setup/IA-API/upload-ai-web/src/App.tsx
@@ -1,7 +1,8 @@
 import { Button } from "./components/ui/button";
-import {Github} from 'lucide-react'
+import {FileVideo, Github} from 'lucide-react'
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+
 
 export function App() {
    return (
@@ -23,12 +24,36 @@ export function App() {
     <main className="flex-1 p-6 flex gap-6 ">
       <div className="flex flex-col flex-1 gap-4 ">
         <div className="grid grid-rows-2 gap-4 flex-1">
-          <Textarea/>
-          <Textarea/>
+          
+          <Textarea 
+            className="resize-none p-5 leading-relaxed" 
+            placeholder="Inclua o prompt para a IA.."
+          />
+          <Textarea 
+            className="resize-none p-5 leading-relaxed"
+            placeholder="Resultado gerado pela IA"
+            readOnly
+          />
+
         </div>
         <p className="text-sm text-muted-foreground">Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{Transcription}'} </code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.   </p>
       </div>
-      <aside className="w-80"></aside>
+      <aside className="w-80 space-y-6">
+        <form className="space-y-6">
+        <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5" >
+          <FileVideo className="h-4 w-4"/>
+          Carregar vídeo
+         
+         </label>
+        <input type="file" id="video" accept="video/mp4" className="sr-only" />
+       
+        <Separator/>
+
+        </form>
+        <div className="space-y-1">
+          
+        </div>
+      </aside>
     </main>
 
     </div>
